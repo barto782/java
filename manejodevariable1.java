c4686759bb6b5759aaaeeb4e62bb9401db603f83
john <202300569j@utea.edu.pe>
2024-06-19 09:37:00 -0500

    gaaaa

---
+++ manejodevariable1.java
@@ -0,0 +1,10 @@
+public class manejodevariable {
+    public static void main(String[] args) {
+        String palabra ="Cerveza";
+        String frase = "¿dónde esta mi cerveza?";
+
+        System.out.println("Palabra que uso en frecuencia "+palabra);
+        System.out.println("Una frase que uso a veces "+frase);
+
+    }
+}
